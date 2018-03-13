08af45a1376cf798d0bcef141aa1df791fff9af6
sicarii-13 <jjungschlager@gmail.com>
2018-03-13 17:56:51 +0100

    Bunny aproves this shit

---
+++ historicstyle/quiz.js
@@ -0,0 +1,136 @@
+/*MIT License
+
+ Copyright (c) 2018 Joeri Jungschlager
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ *
+ *  */
+
+(function () {
+  function buildQuiz() {
+    // we'll need a place to store the HTML output
+    const output = [];
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // we'll want to store the list of answer choices
+      const answers = [];
+
+      // and for each available answer...
+      for (letter in currentQuestion.answers) {
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>`
+      );
+    });
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults() {
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = "lightgreen";
+      } else {
+        // if answer is wrong or blank
+        // color the answers red
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  const quizContainer = document.getElementById("quiz");
+  const resultsContainer = document.getElementById("results");
+  const submitButton = document.getElementById("submit");
+  const myQuestions = [
+    {
+      question: "Wie mag er getapt worden bij de sleepwet?",
+      answers: {
+        a: "De verdachte.",
+        b: "Het gezin waarin de verdacht woont.",
+        c: "De hele wijk waarin de verdacht woont."
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Wat mag er getapt/gehacked worden door de AIVD?",
+      answers: {
+        a: "Smart TV's, Computers, Consoles, Smartphones & tablets.",
+        b: "Pacemakers, Koffiezet apparaten & TV's",
+        c: "Apparaten mogen niet gehacked worden door de AIVD"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "Welke data mag de AIVD verzamelen?",
+      answers: {
+        a: "Inhoud van gesprekken.",
+        b: "Foto's & Metadata.",
+        c: "Er is geen duidelijk kader in de wet.",
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Hoe lang moet deze data bewaard worden?",
+      answers: {
+        a: "6 maanden.",
+        b: "3 jaar.",
+        c: "10 jaar",
+      },
+      correctAnswer: "b"
+    }
+  ];
+  // display quiz right away
+  buildQuiz();
+
+  // on submit, show results
+  submitButton.addEventListener("click", showResults);
+})();
--- quiz.js
+++ quiz.js
@@ -1,83 +1,91 @@
-
 const rivetsData = {
-  prev: function (e, rvData) {
-    const self = rvData || this;
-    const $this = $(e.currentTarget);
-    const index = $this.parent().data('question');
+    prev: function(e, rvData) {
+        const self = rvData || this;
+        const $this = $(e.currentTarget);
+        const index = $this.parent().data('question');
 
-    // first question
-    if (index == 0) {
-      return;
-    }
+        // first question
+        if (index == 0) {
+            return;
+        }
 
-    self.questions[index].active = false;
-    self.questions[index - 1].active = true;
-  },
-
-  next: function (e, rvData) {
-    const self = rvData || this;
-    const $this = $(e.currentTarget);
-    const index = $this.parent().data('question');
-
-    // last question
-    if (index == self.questions.length - 1) {
-      return;
-    }
-
-    self.questions[index].active = false;
-    self.questions[index + 1].active = true;
-  },
-
-  check: function (e, rvData) {
-    const self = rvData || this;
-
-    let correct = 0;
-    for (let i = 0, n = self.questions.length; i < n; i++) {
-      if (self.questions[i].answers.every((answer) => !!answer.chosen === !!answer.accepted)) {
-        correct++;
-      }
-    }
-
-    rivetsData.result = (correct / self.questions.length * 100) + ' %';
-  },
-
-  currentQuestion: 0,
-  result: false,
-  questions: [
-    {
-      active: true,
-      question: 'Wie mag er getapt worden bij de sleepwet?',
-      answers: [
-        { description: 'De verdachte.' },
-        { description: 'Het gezin waarin de verdacht woont.' },
-        { description: 'De hele wijk waarin de verdachte woont.', accepted: true }
-      ]
+        self.questions[index].active = false;
+        self.questions[index - 1].active = true;
     },
-    {
-      question: 'Wat mag er getapt/gehacked worden door de AIVD?',
-      answers: [
-        { description: 'Smart TV\'s, Computers, Consoles, Smartphones & tablets.', accepted: true },
-        { description: 'Pacemakers, Koffiezet apparaten & TV\'s' },
-        { description: 'Apparaten mogen niet gehacked worden door de AIVD' }
-      ]
+
+    next: function(e, rvData) {
+        const self = rvData || this;
+        const $this = $(e.currentTarget);
+        const index = $this.parent().data('question');
+
+        // last question
+        if (index == self.questions.length - 1) {
+            return;
+        }
+
+        self.questions[index].active = false;
+        self.questions[index + 1].active = true;
     },
-    {
-      question: 'Welke data mag de AIVD verzamelen?',
-      answers: [
-        { description: 'Inhoud van gesprekken.' },
-        { description: 'Foto\'s & Metadata.' },
-        { description: 'Er is geen duidelijk kader in de wet.', accepted: true }
-      ]
+
+    check: function(e, rvData) {
+        const self = rvData || this;
+
+        let correct = 0;
+        for (let i = 0, n = self.questions.length; i < n; i++) {
+            if (self.questions[i].answers.every((answer) => !!answer.chosen === !!answer.accepted)) {
+                correct++;
+            }
+        }
+
+        rivetsData.result = (correct / self.questions.length * 100) + ' %';
     },
-    {
-      question: 'Hoe lang moet deze data bewaard worden?',
-      answers: [
-        { description: '6 maanden.' },
-        { description: '3 jaar.', accepted: true },
-        { description: '10 jaar' }
-      ]
-    }
-  ]
+
+    currentQuestion: 0,
+    result: false,
+    questions: [{
+            active: true,
+            question: 'Wie mag er getapt worden bij de sleepwet?',
+            answers: [
+                { description: 'De verdachte.' },
+                { description: 'Het gezin waarin de verdacht woont.' },
+                { description: 'De hele wijk waarin de verdachte woont.', accepted: true }
+            ]
+        },
+        {
+            question: 'Wat mag er getapt/gehacked worden door de AIVD?',
+            answers: [
+                { description: 'Smart TV\'s, Computers, Consoles, Smartphones & tablets.', accepted: true },
+                { description: 'Pacemakers, Koffiezet apparaten & TV\'s' },
+                { description: 'Apparaten mogen niet gehacked worden door de AIVD' }
+            ]
+        },
+        {
+            question: 'Welke orginisaties zijn er tegen de slepwet?',
+            answers: [
+                { description: 'Bits of Freedoms, Raad van Staten, Amnesty International & Computer Club Geulle.', accepted: true },
+                { description: 'AIVD, MIVD' },
+                { description: 'Libertarische Partij, VPNgids, De Partij voor de Dieren, Privacy First', accepted: true }
+            ]
+        },
+        {
+            question: 'Welke data mag de AIVD verzamelen?',
+            answers: [
+                { description: 'Inhoud van gesprekken.' },
+                { description: 'Foto\'s & Metadata.' },
+                { description: 'Er is geen duidelijk kader in de wet.', accepted: true }
+            ]
+        },
+        {
+            question: 'Hoe lang moet deze data bewaard worden?',
+            answers: [
+                { description: '6 maanden.' },
+                { description: '3 jaar.', accepted: true },
+                { description: '10 jaar' }
+            ]
+        }
+
+    ]
+
 };
 
 window.rivetsData = rivetsData;
@@ -86,13 +94,13 @@ rivets.formatters['=='] = (valueA, valueB) => valueA == valueB;
 rivets.formatters['<'] = (valueA, valueB) => valueA < valueB;
 
 rivets.formatters['beforeLast'] = (valueA, valueB) => {
-  return valueA < valueB.length - 1;
+    return valueA < valueB.length - 1;
 };
 
 rivets.formatters['atLast'] = (valueA, valueB) => {
-  return valueA === valueB.length - 1;
+    return valueA === valueB.length - 1;
 };
 
 $(document).ready(() => {
-  rivets.bind($('.quiz-container'), rivetsData);
+    rivets.bind($('.quiz-container'), rivetsData);
 });
